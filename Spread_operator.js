26b175fb0e9c35c5210ce97c1fdc56e3c036d072
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-17 17:49:21 +0530

    Create Spread_operator.js

---
+++ Spread_operator.js
@@ -0,0 +1,9 @@
+const shoppingList = ["eggs", "milk", "butter"];
+const shoppingBasket = [...shoppingList , "candy" , "icecream" , "chocolates"]
+console.log(shoppingBasket)
+
+
+
+
+//output
+[ 'eggs', 'milk', 'butter', 'candy', 'icecream', 'chocolates' ]
