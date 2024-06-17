4835821b341d5808b99f97567d5188e56c923b33
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-06-17 17:28:47 +0530

    Create Object_Literal.js

---
+++ Object_Literal.js
@@ -0,0 +1,18 @@
+function addressMaker(address) {
+    const {city, state} = address;
+    
+    const newAddress = {
+        city,
+        state,
+        country: 'United States'
+    };
+    console.log(`${newAddress.city}, ${newAddress.state}, ${newAddress.country}`)
+}
+
+addressMaker({city: 'Austin', state: 'Texas'});
+
+
+
+
+//output
+Austin, Texas, United States
